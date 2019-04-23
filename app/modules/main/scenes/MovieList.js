b9a54da324b62c337bdc1b046c9d7ac2e6c16e8d
Lucas Carmona <lucascarmona@MacBook-Pro-de-Lucas.local>
2019-04-23 16:55:21 -0300

    fix broke list

--- app/modules/main/scenes/MovieList.js
+++ app/modules/main/scenes/MovieList.js
@@ -16,10 +16,6 @@ import {Actions} from "react-native-router-flux";
 
 
 const device_width = Dimensions.get('window').width;
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 const { loadMatchs, loadMatch } = actions;
 
 class MovieList extends React.Component {
