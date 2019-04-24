ba6dab03016c9e3153fc60ecd8d8c0fbed556633
Alan Souto <alaned.souto@gmail.com>
2019-04-24 12:49:58 -0300

    OK modal comments

--- app/modules/main/scenes/Detail.js
+++ app/modules/main/scenes/Detail.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Dimensions, StyleSheet, View, ScrollView, TouchableOpacity, Text } from "react-native";
-
-import { ListItem } from 'react-native-elements';
+import { Dimensions, StyleSheet, View, ScrollView, TouchableOpacity, Text, Modal, Button } from "react-native";
+//import { TextInput } from 'react-native-paper';
+import { ListItem, FormInput, FormLabel } from 'react-native-elements';
 import PosterCmp from '../components/Poster';
 import { actions } from "../"
 
@@ -47,10 +47,20 @@ const list = [
     },
 ]
 
+
+
 class MoviePoster extends Component {
     constructor() {
         super();
         this.buildPoster = this.buildPoster.bind(this);
+        this.buildModal = this.buildModal.bind(this);
+        this.addComment = this.addComment.bind(this);
+        this.closeComment = this.closeComment.bind(this);
+        this.state = {
+            isModalVisible: false,
+            value: 'Ingresa tu comentario'
+        };
+
     }
 
     componentDidMount() {
@@ -82,7 +92,16 @@ class MoviePoster extends Component {
     }
     addComment() {
 
+        this.setState({ isModalVisible: !this.state.isModalVisible });
+
+
     }
+
+    closeComment() {
+        this.setState({ isModalVisible: false });
+    }
+
+
 
     buildPoster() {
         const { Poster, Title, description, Runtime, Year } = this.props.currentMovie;
@@ -127,19 +146,52 @@ class MoviePoster extends Component {
             <ScrollView style={styles.container}>
                 {this.props.currentMovie && this.buildPoster()}
                 {this.buildComments()}
+                {this.buildModal()}
             </ScrollView>
         )
     }
 
-
+    buildModal() {
+        return (
+                <Modal visible={this.state.isModalVisible} animationType="fade" transparent={true}>
+                    <View style={styles.modal}>
+                        <View style={{width: 280, height:220}}>
+                        <FormLabel></FormLabel>
+                            <FormInput
+                                inputStyle={styles.textInput}
+                                multiline= {true}
+                                placeholder= 'Ingresa tu comentario...'
+                                textarea value={this.state.value}
+                                onChangeText={(text) => this.setState({ text })}
+                                value={this.state.text}
+                            />
+                            <Button
+                                onPress={() => this.closeComment()}
+                                title="Cerrar"
+                            >
+                            </Button>
+                            <Button
+                                onPress={() => this.closeComment()}
+                                title="Guardar"
+                            >
+                            </Button>
+                        </View>
+                    </View>
+                </Modal>
+        )
+    }
 }
+
+
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
     },
-    button: {},
+    button: {
+    },
     rate: {
         flexDirection: 'row',
     },
@@ -148,6 +200,31 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
     },
     listcontainer: {
+    },
+
+    modal: {
+        height: height*0.5,
+        width: width*0.9,
+        position: 'absolute',
+        top: height*0.2,
+        left: width*0.05,
+        backgroundColor: '#f1f1f1',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+        shadowColor: 'black',
+        shadowOpacity: 5.0,
+    },
+
+    textInput: {
+        height: 100,
+        width: 250,
+        borderColor: 'gray',
+        borderWidth: 1,
+        /*autoCapitalize:'sentences',
+        blurOnSubmit:'true'
+        */
     }
 
 });
